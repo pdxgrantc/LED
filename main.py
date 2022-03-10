cd65133a1f11d972c560b9b9a1b5fba8ba963303
pdxgrantc <pdxgrant@gmail.com>
2022-03-10 09:19:43 -0800

    readin colors

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 import os
 import json
 
-workDir = os.getcwd()
- 
-path = workDir + "/database/data/color.json"
+
 # Opening JSON file
-with open(path, 'r') as myfile:
-    data=myfile.read()
 
+class Data:
+  def __init__(self):
+    workDir = os.getcwd()
+    self.path = workDir + "/database/data/color.json"
+    self.red
+    self.green
+    self.blue
 
-jsonOBJ = json.loads(data)
+    jsonOBJ = json.loads(data)
 
-print(jsonOBJ["redVal"])
-print(jsonOBJ["greenVal"])
-print(jsonOBJ["blueVal"])
+    def readColors(self):
+        with open(self.path, 'r') as myfile:
+            data=myfile.read()
+
+        jsonOBJ = json.loads(data)
+
+        self.red = jsonOBJ["redVal"]
+        self.green = jsonOBJ["greenVal"]
+        self.blue = jsonOBJ["blueVal"]
