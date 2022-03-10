3a48f5ab9d853bccb1e6b8de6ac804be3e882ffc
Grant Conklin <pdxgrantc@gmail.com>
2022-03-10 10:01:56 -0800

    color

--- main.py
+++ main.py
@@ -28,10 +28,10 @@ class Data:
         print(self.blue)
 
 def main():
-    data = Data
+    color = Data
 
     #data.readColors()
-    data.display()
+    color.display()
 
     while True:
         pass
