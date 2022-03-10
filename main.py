ddafc2147f9355e327348e255e12942958e2bc80
pdxgrantc <pdxgrant@gmail.com>
2022-03-10 09:30:26 -0800

    who knows

--- main.py
+++ main.py
@@ -8,11 +8,9 @@ class Data:
   def __init__(self):
     workDir = os.getcwd()
     self.path = workDir + "/database/data/color.json"
-    self.red
-    self.green
-    self.blue
-
-    jsonOBJ = json.loads(data)
+    self.red = 0
+    self.green = 0
+    self.blue = 0
 
     def readColors(self):
         with open(self.path, 'r') as myfile:
@@ -23,3 +21,22 @@ class Data:
         self.red = jsonOBJ["redVal"]
         self.green = jsonOBJ["greenVal"]
         self.blue = jsonOBJ["blueVal"]
+
+    def display(self):
+        print(self.red)
+        print(self.green)
+        print(self.blue)
+
+
+
+def main():
+    data = Data
+
+    #data.readColors()
+    data.display()
+
+    while True:
+        pass
+
+if __name__=='__main__':
+    main()
